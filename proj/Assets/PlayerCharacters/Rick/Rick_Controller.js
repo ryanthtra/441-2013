cf7f040d241742279e08d6cb7324093f90a23132
David Turner <csusbdt@gmail.com>
2013-06-06 13:20:52 -0700

    added collider

--- proj/Assets/PlayerCharacters/Rick/Rick_Controller.js
+++ proj/Assets/PlayerCharacters/Rick/Rick_Controller.js
@@ -1,6 +1,10 @@
 #pragma strict
 
+private var controller : CharacterController = null;
+private var forwardVelocity: Vector2 = new Vector2(-0.2, 0);
+
 function Start () {
+  controller = collider as CharacterController;
 }
 
 function Update () {
@@ -8,6 +12,7 @@ function Update () {
     GetComponent(Rick_Anim).attack();
   } else if (Input.GetAxis ("Horizontal") > 0.001) {
     GetComponent(Rick_Anim).walk();
+    controller.SimpleMove(forwardVelocity);
   } else {
     GetComponent(Rick_Anim).idle();
   }
