54239e2915c96f7d4e4c6bd68e80559299121381
David Turner <csusbdt@gmail.com>
2013-06-06 06:20:59 -0700

    developed anim system a little

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 - [Course Wiki](https://github.com/csusbdt/441-2013/wiki)
 - [Syllabus](http://csusbdt.github.com/441-2013)
 
+## Proj
+
+The proj folder contains a Unity project,
+which contains an illustration of how to 
+accomplish 2D animation in Unity.
+
+See [Which Kind Of Array Or Collection Should I Use?](http://wiki.unity3d.com/index.php?title=Which_Kind_Of_Array_Or_Collection_Should_I_Use?)
+for useful discussion on arrays in Unity.
+
---
+++ proj/Assets/AnimSystem/SpritesheetOffsets.js
@@ -0,0 +1,37 @@
+#pragma strict
+
+// sprite sheet with 8 columns and 3 rows
+// row 0 (bottom row)
+static var c8r3_0 : Vector2[] = [ 
+  Vector2(.000, 0), 
+  Vector2(.125, 0),
+  Vector2(.250, 0),
+  Vector2(.375, 0),
+  Vector2(.500, 0),
+  Vector2(.625, 0),
+  Vector2(.750, 0),
+  Vector2(.875, 0)
+];
+// row 1
+static var c8r3_1 : Vector2[] = [ 
+  Vector2(.000, .3333333), 
+  Vector2(.125, .3333333),
+  Vector2(.250, .3333333),
+  Vector2(.375, .3333333),
+  Vector2(.500, .3333333),
+  Vector2(.625, .3333333),
+  Vector2(.750, .3333333),
+  Vector2(.875, .3333333)
+];
+// row 2
+static var c8r3_2 : Vector2[] = [ 
+  Vector2(.000, .6666666), 
+  Vector2(.125, .6666666),
+  Vector2(.250, .6666666),
+  Vector2(.375, .6666666),
+  Vector2(.500, .6666666),
+  Vector2(.625, .6666666),
+  Vector2(.750, .6666666),
+  Vector2(.875, .6666666)
+];
+
